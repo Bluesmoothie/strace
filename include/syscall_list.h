ab9eed2fa8e9e653f746d8fa4c8bf985bdb398a7
Yves Gille <mail@yvesgille.fr>
2026-01-15 14:50:25 +0100

    Added type STRINGTAB and added definition for execve Now indexing list

--- include/syscall_list.h
+++ include/syscall_list.h
@@ -28,7 +28,8 @@ typedef enum	e_type {
 	TYPE_SIZE_T,
 	TYPE_INT,
 	TYPE_POINTER,
-	TYPE_STRING
+	TYPE_STRING,
+	TYPE_STRINGTAB
 }	t_type;
 
 typedef struct	s_syscall {
@@ -40,7 +41,7 @@ typedef struct	s_syscall {
 }	t_syscall;
 
 const t_syscall	SYSCALL_LIST[SYSCALL_MAX + 1] = {
-	{
+	[0] = {
 		//ssize_t read(int fd, void* buf, size_t count)
 		.name = "read",
 		.return_type = TYPE_SSIZE_T,
@@ -52,7 +53,7 @@ const t_syscall	SYSCALL_LIST[SYSCALL_MAX + 1] = {
 			TYPE_SIZE_T
 		}
 	},
-	{
+	[1] = {
 		//ssize_t write(int fd, const void* buf, size_t count)
 		.name = "write",
 		.return_type = TYPE_SSIZE_T,
@@ -64,7 +65,7 @@ const t_syscall	SYSCALL_LIST[SYSCALL_MAX + 1] = {
 			TYPE_SIZE_T
 		}
 	},
-	{
+	[2] = {
 		//int open(const char *path, int flags, umode_t mode) Mode only if O_CREAT or O_TMPFILE in flags
 		.name = "open",
 		.return_type = TYPE_INT,
@@ -76,7 +77,7 @@ const t_syscall	SYSCALL_LIST[SYSCALL_MAX + 1] = {
 			TYPE_INT
 		}
 	},
-	{
+	[3] = {
 		//int close(int fd)
 		.name = "close",
 		.return_type = TYPE_INT,
@@ -84,6 +85,18 @@ const t_syscall	SYSCALL_LIST[SYSCALL_MAX + 1] = {
 		.arg_count = 1,
 		.arg_type = {
 			TYPE_INT
+		}
+	},
+	[59] = {
+		//int execve(const char *path, char *const _Nullable argv[], char *const _Nullable envp[]);
+		.name = "execve",
+		.return_type = TYPE_INT,
+		.set_errno = true,
+		.arg_count = 3,
+		.arg_type = {
+			TYPE_STRING,
+			TYPE_STRINGTAB,
+			TYPE_STRINGTAB
 		}
 	}
 };

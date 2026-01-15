66c50bbf03565a1bc56e3106c624eeec9fc564ef
Yves Gille <mail@yvesgille.fr>
2026-01-15 14:58:53 +0100

    Removed set_errno properti since it seems that all syscalls set errno when returning -1

--- include/syscall_list.h
+++ include/syscall_list.h
@@ -29,13 +29,13 @@ typedef enum	e_type {
 	TYPE_INT,
 	TYPE_POINTER,
 	TYPE_STRING,
-	TYPE_STRINGTAB
+	TYPE_STRINGTAB,
+	TYPE_LONG
 }	t_type;
 
 typedef struct	s_syscall {
 	char*	name;
 	t_type	return_type;
-	bool	set_errno;
 	short	arg_count;
 	t_type	arg_type[6];
 }	t_syscall;
@@ -45,7 +45,6 @@ const t_syscall	SYSCALL_LIST[SYSCALL_MAX + 1] = {
 		//ssize_t read(int fd, void* buf, size_t count)
 		.name = "read",
 		.return_type = TYPE_SSIZE_T,
-		.set_errno = true,
 		.arg_count = 3,
 		.arg_type = {
 			TYPE_INT,
@@ -57,7 +56,6 @@ const t_syscall	SYSCALL_LIST[SYSCALL_MAX + 1] = {
 		//ssize_t write(int fd, const void* buf, size_t count)
 		.name = "write",
 		.return_type = TYPE_SSIZE_T,
-		.set_errno = true,
 		.arg_count = 3,
 		.arg_type = {
 			TYPE_INT,
@@ -69,7 +67,6 @@ const t_syscall	SYSCALL_LIST[SYSCALL_MAX + 1] = {
 		//int open(const char *path, int flags, umode_t mode) Mode only if O_CREAT or O_TMPFILE in flags
 		.name = "open",
 		.return_type = TYPE_INT,
-		.set_errno = true,
 		.arg_count = 3,
 		.arg_type = {
 			TYPE_STRING,
@@ -81,17 +78,27 @@ const t_syscall	SYSCALL_LIST[SYSCALL_MAX + 1] = {
 		//int close(int fd)
 		.name = "close",
 		.return_type = TYPE_INT,
-		.set_errno = true,
 		.arg_count = 1,
 		.arg_type = {
 			TYPE_INT
 		}
 	},
+	[14] = {
+		//long rt_sigprocmask(int how, const sigset_t __user *set, sigset_t __user *oset, size_t sigsetsize)
+		.name = "rt_sigprocmask",
+		.return_type = TYPE_LONG,
+		.arg_count = 4,
+		.arg_type = {
+			TYPE_INT,
+			TYPE_POINTER,
+			TYPE_POINTER,
+			TYPE_SIZE_T
+		}
+	},
 	[59] = {
-		//int execve(const char *path, char *const _Nullable argv[], char *const _Nullable envp[]);
+		//int execve(const char *path, char *const _Nullable argv[], char *const _Nullable envp[])
 		.name = "execve",
 		.return_type = TYPE_INT,
-		.set_errno = true,
 		.arg_count = 3,
 		.arg_type = {
 			TYPE_STRING,
